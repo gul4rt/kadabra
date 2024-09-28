a0af3e80b064f9d04b1afc7e274b06cc93722dbb
Samuel Gulart <samuelgulart@outlook.com>
2024-09-28 14:06:19 -0300

    calling check_package and adding output feature

--- kadabra.py
+++ kadabra.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-from package_parser import *
-from internals import *
+from core.package_parser import *
+from core.internals import *
 from sys import argv
 
 print(banner)
@@ -12,9 +12,34 @@ args = parser.parse_args()
 try:
     f = open(args.file)
     f_content = f.read()
-    json_parser(f_content)
+    data = dependencies_parser(f_content)
     f.close()
 except: 
     print("An error occurred while trying to open the file.")
 
+print("[i] checking dependencies\n")
+finds = 0
 
+if args.output:
+    try:
+        f = open(args.output, 'a')
+    except:
+        print("An error occurred while trying to open the output file.")
+
+for i in data.keys():
+    try:
+        r = check_package(i)
+        if(r == False):
+            print(f"[404] {i} package not found, possibly vulnerable to dependency confusion")
+            finds += 1
+            if args.output:
+                f.write(i+'\n')
+        elif(r == None):
+            print("[!] error in {i} package search")
+        else:
+            print(f"[200] {i} package is up")
+    except:
+        print("[!] error")
+
+f.close()
+print(f"[i] total finds: {finds}")
